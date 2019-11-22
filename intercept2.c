bc26bfb4be3c4b64c43c38348453b03d5c719b5e
angelgbriel <angelgbriel@gmail.com>
2019-11-21 22:16:46 -0600

    m2

--- intercept2.c
+++ intercept2.c
@@ -17,7 +17,7 @@ MODULE_LICENSE("GPL");
 // tomar la direccion de sys_call_table
 //realizar cada vez que se reinicie el sistema puesto que la direccion cambia
 
-unsigned long *sys_call_table = (unsigned long*)0xffffffffbc000200;
+unsigned long *sys_call_table = (unsigned long*)0xffffffffb5c00200;
 
 //puntero de la funcion del sys_openat
 asmlinkage int (*real_unlinkat)(int DirFileDescriptor, char* path, int Flag);
